b274ffc9a9e083ff8e6aabac475ac07c64c6fed6
jatinchaudhary20 <chaudharyjatin205@gmail.com>
2023-11-23 21:48:25 +0530

    design the recycler view layout of pateint list

---
+++ app/src/main/java/com/example/medreport_care/doctor/docpateintlist.kt
@@ -0,0 +1,12 @@
+package com.example.medreport_care.doctor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.medreport_care.R
+
+class docpateintlist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_docpateintlist)
+    }
+}
